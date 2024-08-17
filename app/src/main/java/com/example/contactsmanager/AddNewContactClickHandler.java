0e920b3568350366687a616fc9d320392b6adca4
Chayan Pal <chayan110201@gmail.com>
2024-08-17 18:13:45 +0530

    Click handler added to add new contact activity

---
+++ app/src/main/java/com/example/contactsmanager/AddNewContactClickHandler.java
@@ -0,0 +1,32 @@
+package com.example.contactsmanager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+
+public class AddNewContactClickHandler {
+    Contacts contact;
+    Context context;
+    MyViewModel myViewModel;
+
+    public AddNewContactClickHandler(Contacts contact, Context context, MyViewModel myViewModel) {
+        this.contact = contact;
+        this.context = context;
+        this.myViewModel = myViewModel;
+    }
+
+    public void onSubmitBtnClicked(View view){
+        if (contact.getName()==null || contact.getPhone()==null || contact.getEmail()==null){
+            Toast.makeText(context, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent i = new Intent(context, MainActivity.class);
+
+            Contacts c = new Contacts(contact.getName(), contact.getPhone(), contact.getEmail());
+
+            myViewModel.addNewContact(c);
+
+            context.startActivity(i);
+        }
+    }
+}
